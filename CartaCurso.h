f5a9cd61c1f6bde4f6e9315358837dfc89e4fabd
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2016-08-30 16:33:23 -0300

    atualizados headers

--- CartaCurso.h
+++ CartaCurso.h
@@ -2,11 +2,11 @@
 #ifndef _CARTACURSO_H_INCLUDED_
 #define _CARTACURSO_H_INCLUDED_
 
-struct CartaCurso{
-char * nombre, * descripcion;
-int ataque, defensa;
-void (*reprobar)(void *, void *); //Los parametros son la carta y el enemigo
-void (*aprobar)(void *, void *); //Los parametros son la carta y el jugador
-};
+typedef struct CartaCurso{
+	char * nombre, * descripcion;
+	int ataque, defensa;
+	void (*reprobar)(void *, void *); //Los parametros son la carta y el enemigo
+	void (*aprobar)(void *, void *); //Los parametros son la carta y el jugador
+} CartaCurso;
 
 #endif
--- lista.h
+++ lista.h
@@ -1,1 +1,25 @@
 //Archivo con estructuras y prototipos de funciones de lista enlazada
+#include "CartaCurso.h"
+#ifndef _LISTA_H_INCLUDED_
+#define _LISTA_H_INCLUDED_
+
+typedef struct node {               // Estructura nodo de lista enlazada
+	CartaCurso carta;
+    struct node *prev;
+    struct node *next;
+} tNodo;
+
+typedef struct lista {              // Estructura lista
+    tNodo *head;                    // Almacena la cabeza de la lista enlazada
+    int size;
+    unsigned int pos_actual;
+} tLista;
+
+void initialize(tLista *lista);
+void insert(tLista *lista, CartaCurso Carta, unsigned int pos);
+void delete(tLista *lista, unsigned int pos)
+void moveToPos(tLista *lista, unsigned int posicion);
+CartaCurso getValue(tLista *lista);
+void free_linked(tLista *base);
+
+#endif
